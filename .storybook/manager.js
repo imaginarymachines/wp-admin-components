393f34f491158a57d66dd4700f34efd63a1cf9ec
Josh Pollock <jpollock412@gmail.com>
2021-11-29 20:03:45 -0500

    manager

---
+++ .storybook/manager.js
@@ -0,0 +1,24 @@
+import { addons } from '@storybook/addons';
+//https://storybook.js.org/docs/react/configure/features-and-behavior
+addons.setConfig({
+  isFullscreen: false,
+  showNav: true,
+  showPanel: true,
+  panelPosition: 'bottom',
+  enableShortcuts: true,
+  isToolshown: true,
+  theme: undefined,
+  selectedPanel: 'docs',
+  initialActive: 'sidebar',
+  sidebar: {
+    showRoots: false,
+    collapsedRoots: ['other'],
+  },
+  toolbar: {
+    title: { hidden: false },
+    zoom: { hidden: false },
+    eject: { hidden: false },
+    copy: { hidden: false },
+    fullscreen: { hidden: false },
+  },
+});
