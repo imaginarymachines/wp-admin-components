bbbe0a801c9a1729686a5415fe47c1ffa6822501
Josh Pollock <jpollock412@gmail.com>
2021-11-30 11:32:30 -0500

    More button tests

--- tests/Button.test.tsx
+++ tests/Button.test.tsx
@@ -8,3 +8,16 @@ it( 'Renders default button', () => {
 it( 'Renders default link', () => {
     snapshot(<Button a={'https://hiroy.club'}>ClickMe</Button>);
 });
+
+
+it( 'Renders secondary button', () => {
+    snapshot(<Button onClick={() =>{}} variant="secondary">Secondary</Button>);
+});
+
+it( 'Renders small button', () => {
+    snapshot(<Button onClick={() =>{}} isSmall={true}>Small</Button>);
+});
+
+it( 'Renders hero button', () => {
+    snapshot(<Button onClick={() =>{}} isHero={true}>Hero</Button>);
+});
