5d141a38021a0a5f055c731019b286fb152aff86
Josh Pollock <jpollock412@gmail.com>
2021-11-30 13:39:38 -0500

    Docs

--- stories/FormTable.stories.tsx
+++ stories/FormTable.stories.tsx
@@ -80,22 +80,7 @@ const Test : FC<FormProps> = ({id,onSubmit}) => {
 
   )
 }
-const Template: Story<FormProps> = props => (<Form id={props.id} onSubmit={() => {}}>
-    <FormTable >
-        <>
-            <TrInput
-                {...inputProps}
-            />
-            <TrSelect
-                {...selectProps}
-            />
-            <TrSubmitButton
-                {...submitProps}
-            />
-        </>
-    </FormTable>
-  </Form>
-);
+const Template: Story<FormProps> = props => (<Test {...props}/>);
 
 // By passing using the Args format for exported stories, you can control the props for a component for reuse in a test
 // https://storybook.js.org/docs/react/workflows/unit-testing
