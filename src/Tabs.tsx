1b0196c01d2ba48c327d6d4cfc47042a344972cc
Josh Pollock <jpollock412@gmail.com>
2021-11-29 20:36:55 -0500

    Functional tabs

--- src/Tabs.tsx
+++ src/Tabs.tsx
@@ -1,9 +1,11 @@
-import React, { FC, HTMLAttributes, ReactChild,useMemo } from 'react';
+import React, { FC, HTMLAttributes, ReactChild,useMemo, useState } from 'react';
 
 
-export interface TabProps extends HTMLAttributes<HTMLDivElement> {
+export interface TabProps {
   id:string;
   children:ReactChild;
+  label:string;
+
 }
 
 
@@ -13,27 +15,53 @@ export const Tab :FC<TabProps> = ({id,children}) => {
   )
 }
 
-export interface TabButtonProps extends HTMLAttributes<HTMLDivElement> {
+export interface TabButtonProps  {
   id:string;
-  children:ReactChild;
+  label:ReactChild;
+  isSelected:boolean;
+  onClick:(id:string)=>void
 }
 
 
-export const TabButton :FC<TabButtonProps> = ({id,children}) => {
+export const TabButton :FC<TabButtonProps> = ({id,label,isSelected,onClick}) => {
   return(
-    <li><a href={`#${id}`}>{children}</a></li>
+    <li id={`tab-button-${id}`} aria-controls={`tab-${id}`}  aria-selected={isSelected}>
+      <a
+        href={`#${id}`}
+        onClick={(e) => {
+          e.preventDefault();
+          onClick(id);
+        }}
+      >
+        {label}
+      </a>
+    </li>
   )
 }
 
 export interface TabContentProps extends HTMLAttributes<HTMLDivElement> {
   id:string;
   children:ReactChild;
+  isSelected:boolean;
 }
 
 
-export const TabContent :FC<TabContentProps> = ({id,children}) => {
+export const TabContent :FC<TabContentProps> = ({id,children,isSelected}) => {
   return(
-    <div id={`#${id}`}>{children}</div>
+    <div id={`tab-${id}`}
+      role="tabpanel"
+      aria-labelledby={`tab-button-${id}`}
+      aria-display={isSelected}
+      style={isSelected ? {
+        display:'block',
+        visibility:'visible'
+      }:{
+        display:'none',
+        visibility:'hidden'
+      }}
+    >
+      {children}
+    </div>
   )
 }
 
@@ -44,18 +72,44 @@ export interface TabsProps extends HTMLAttributes<HTMLDivElement> {
 
 
 const Tabs: FC<TabsProps> = (props) => {
-  const tabs = useMemo( () => {
+  const tabs = [
+    {id: 'one', children:<div>Tab One Content</div>,label:'One'},
+    {id: 'two', children:<div>Tab Two Content</div>,label:'Two'}
+  ];
 
-    //return props.tabs;
-    return [
-      {id: 'one', children:<div>One</div>}
-    ]
-  },[props.tabs])
+  const [selectedTab,setSelectedTab] = useState<string|null>(() =>{
+    if(! tabs.length){
+      return null;
+    }
+    return tabs[0].id;
+  });
+
+  const isSelectedTab = (tab:TabProps) => selectedTab === tab.id;
    return(
     <div id={props.id}>
-      <ul>
-        {tabs.map((tab:TabProps) =><TabButton key={tab.id}  id={tab.id}>{tab.children}</TabButton> )}
+      <ul role="tablist">
+        {tabs.map((tab:TabProps) =>(
+          <TabButton
+              key={tab.id}
+              isSelected={isSelectedTab(tab)}
+              id={tab.id}
+              label={tab.label}
+              onClick={setSelectedTab}
+           />
+           )
+        )}
       </ul>
+      <>
+      {tabs.map((tab:TabProps) =>(
+          <TabContent
+              key={tab.id}
+              isSelected={isSelectedTab(tab)}
+              id={tab.id}
+           >
+             {tab.children}
+            </TabContent> )
+        )}
+      </>
     </div>
 
    )
